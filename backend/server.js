0dc6a8f6563deec200794a592432387b541f9aa6
Samiksha Wankar <94747339+Samiksha-Wankar@users.noreply.github.com>
2024-09-28 14:04:42 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ app.use(express.json());
 
 app.use('/api/risks', riskRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
